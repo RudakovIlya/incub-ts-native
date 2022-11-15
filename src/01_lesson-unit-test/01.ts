35b4f4b752f23787c92ba6c452abc7c0a0569360
ilya <rudakov.alhan@gmail.com>
2022-11-15 11:50:37 +0300

    initial commit ts-native lessons

---
+++ src/01_lesson-unit-test/01.ts
@@ -0,0 +1,11 @@
+export const sum = (a: number, b: number) => {
+    return a + b;
+};
+
+export const mult = (a: number, b: number) => {
+    return a * b;
+};
+
+export function splitIntoWords(sentence: string) {
+    return sentence.toLowerCase().split(/\s/);
+}
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {splitIntoWords} from "./01_lesson-unit-test/01";
+
+const sentence = 'Hello my friends!'
+splitIntoWords(sentence);
+
+function App() {
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default App;
